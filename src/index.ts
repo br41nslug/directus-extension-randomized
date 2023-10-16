35d299b7f41359620384046ec02604c67c3f2142
Sandros94 <sandro.circi@digitoolmedia.com>
2023-10-16 20:03:42 +0200

    fix: correctly shuffling limited query

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,15 @@ function shuffleArray(input: any[]): any[] {
 	return input;
 }
 function getRandomItems(input: any[], amount: number): any[] {
-	let m = Math.min(input.length), t, i;
+	let l = input.length, m = Math.min(amount, l), t, i;
 	while (m) {
-		i = Math.floor(Math.random() * m--);
-		t = input[m];
-		input[m] = input[i];
+		i = Math.floor(Math.random() * l--);
+		t = input[l];
+		input[l] = input[i];
 		input[i] = t;
+		m--;
 	}
-	return input.slice(0, Math.min(amount, input.length));
+	return input.slice(l);
 }
 
 export default defineEndpoint({
@@ -26,21 +27,21 @@ export default defineEndpoint({
 	handler: (router, { services }) => {
 		const { ItemsService } = services;
 		router.get('/:collection', async (_req: any, res: any) => {
-			const itemService = new ItemsService(_req.params.collection, { 
+			const itemService = new ItemsService(_req.params.collection, {
 				schema: _req.schema,
 				accountability: _req.accountability
 			});
 			try {
 				if (+_req.sanitizedQuery?.limit > 0) {
 					const limit: number = _req.sanitizedQuery.limit;
-					const ids = getRandomItems(await itemService.readByQuery({ 
-						..._req.sanitizedQuery, limit: -1, fields: ['id'] 
+					const ids = getRandomItems(await itemService.readByQuery({
+						..._req.sanitizedQuery, limit: -1, fields: ['id']
 					}), limit).map(({ id }) => id);
-					const filter = _req.sanitizedQuery.filter ? 
+					const filter = _req.sanitizedQuery.filter ?
 						{ _and: [{ id: { _in: ids } }, _req.sanitizedQuery.filter] } :
 						{ id: { _in: ids } }
 					const data = await itemService.readByQuery({ ..._req.sanitizedQuery, filter });
-					res.json(data);
+					res.json(shuffleArray(data));
 				} else {
 					const data = await itemService.readByQuery(_req.sanitizedQuery);
 					res.json(shuffleArray(data));
